8ecc722f9d2a24e43f6e28bae6f93f0216cf009c
vagrant <vagrant@localhost.localdomain>
2018-09-30 09:57:11 +0000

    first cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'vim-enhanced'
+
+package 'tree' do
+   action :install
+end
+
+package 'git' do
+   action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+   content 'This server is the property of Teekay'
+   action :create
+   owner 'root'
+   group 'root'
+end
+
+service 'ntpd' do
+   action [ :enable, :start ]
+end
